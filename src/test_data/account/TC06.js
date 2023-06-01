b8525d946164a0a43611d5e39e1859bec94a3d4a
Curry Pham <khoapd2000@gmail.com>
2023-06-02 06:27:31 +0700

    Customize test data

---
+++ src/test_data/account/TC06.js
@@ -0,0 +1,14 @@
+const INVALID_CRED = {
+    email: 'test',
+    password:'123456'
+}
+const VALID_CRED = {
+    email: '...',
+    password:'...'
+}
+
+// exports.INVALID_CRED = INVALID_CRED
+exports.loginData = {
+    INVALID_CRED,
+    VALID_CRED
+}
--- src/test_script/account/TC_06.js
+++ src/test_script/account/TC_06.js
@@ -1,21 +1,27 @@
 let LoginFlow = require('../../test_flows/account/LoginFlow')
-
-let LoginPage = require('../../pageobjects/account/Login');
+// let testData = require ('../../test_data/account/TC06')
+let {loginData} = require ('../../test_data/account/TC06')
 
 describe('Account', () => {
     
     it('should use inherited PO', async () => {
-        let email = 'test'
-        let password = '123456'
 
         let loginFlow = new LoginFlow()
 
         const LOGIN_ICON = '~Login'
         await $(LOGIN_ICON).click()
 
+        // const {INVALID_CRED} = testData.loginData
+        // const {email, password} = INVALID_CRED
+
+        // const {INVALID_CRED} = loginData
+        const {email, password} = loginData.VALID_CRED
+
+        // const {email, password} = loginData
+
         await loginFlow.login_with_credentials(email, password)
         await loginFlow.verify_login(email)
-        
+
         await browser.pause(3000)
     });
 });
